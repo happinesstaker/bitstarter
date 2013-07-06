1f57f921166f547ca9b5cb8ee5e21b2f4246b65e
John Smith <example@stanford.edu>
2013-07-06 06:40:29 +0000

    Modified grader.js to support url command

--- grader.js
+++ grader.js
@@ -65,10 +65,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_of_html>','URL of index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url){
+	var rest = require('restler');
+	rest.get(program.url).on('complete',function(result){
+	    fs.writeFileSync("temp.html",result);
+            var checkJson = checkHtmlFile("temp.html",program.checks);
+	    var outJson = JSON.stringify(checkJson, null ,4);
+	    console.log(outJson);
+	    });
+    }else{
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
